e6b0ac8be6f681c6641884ef95e6dd9c180c919f
Peter Vondras <peter.vondras@gmail.com>
2017-12-20 23:30:03 -0500

    only import MultiQueue and Priority from duplicate_aware_scheduling

--- common/dstage/test_multiqueue.cpp
+++ common/dstage/test_multiqueue.cpp
@@ -3,11 +3,16 @@
 
 #include <cassert>
 #include <cstdlib>  // EXIT_SUCCESS and EXIT_FAILURE
-namespace {
-unsigned kNumberOfQueues = 3;
 
-using namespace duplicate_aware_scheduling;
-// using duplicate_aware_scheduling::Job;
+namespace {
+
+using duplicate_aware_scheduling::MultiQueue;
+using duplicate_aware_scheduling::Priority;
+
+unsigned kNumberOfQueues = 3;
+unsigned kDecoyA = 500;
+unsigned kDecoyB = kDecoyA + 1;
+unsigned kRealValue = kDecoyB + 1;
 
 }  // namespace
 
@@ -15,33 +20,30 @@ int main() {
   bool success = true;
   fprintf(stderr, "test_multiqueue... ");
 
-  // JobIdFactory j_factory(0);
-  // Job<unsigned> job(0, j_factory.CreateJobId(), 0, 3);
-
   MultiQueue prio_qs(kNumberOfQueues);
 
-  prio_qs.Enqueue(100, std::vector<Priority>{0, 1, 2});
+  prio_qs.Enqueue(kDecoyA, std::vector<Priority>{0, 1, 2});
 
-  prio_qs.Enqueue(200, std::vector<Priority>{0, 1, 2});
+  prio_qs.Enqueue(kRealValue, std::vector<Priority>{0, 1, 2});
 
-  prio_qs.Enqueue(300, std::vector<Priority>{0, 1, 2});
+  prio_qs.Enqueue(kDecoyB, std::vector<Priority>{0, 1, 2});
 
   prio_qs.Enqueue(0, std::vector<Priority>{0});
   prio_qs.Enqueue(1, std::vector<Priority>{1});
   prio_qs.Enqueue(2, std::vector<Priority>{2});
 
-  prio_qs.Purge(100);
-  prio_qs.Purge(300);
+  prio_qs.Purge(kDecoyA);
+  prio_qs.Purge(kDecoyB);
 
   for (unsigned i = 0; i < kNumberOfQueues; i++) {
-    assert(prio_qs.Dequeue(i) == 200);
+    assert(prio_qs.Dequeue(i) == kRealValue);
     assert(prio_qs.Dequeue(i) == i);
   }
 
   if (success) {
-    printf(" Passed\n");
+    fprintf(stderr, " Passed\n");
     return 0;
   } else
-    printf(" Failed\n");
+    fprintf(stderr, " Failed\n");
   return 1;
 }
